80843404dce63a47f6fd51c029e49a8ff20ddbed
Thibaud RENAUX <thibaud.renaux@gmail.com>
2021-07-22 16:24:30 +0200

    fix usage of cryptrauthorization

--- cryptr_django/settings.py
+++ cryptr_django/settings.py
@@ -64,9 +64,9 @@ CRYPTR_CONFIG = {
 
 JWT_AUTH = {
     'JWT_PAYLOAD_GET_USERNAME_HANDLER':
-        'auth0authorization.utils.jwt_get_username_from_payload_handler',
+        'cryptrauthorization.utils.jwt_get_username_from_payload_handler',
     'JWT_DECODE_HANDLER':
-        'auth0authorization.utils.jwt_decode_token',
+        'cryptrauthorization.utils.jwt_decode_token',
     'JWT_ALGORITHM': 'RS256',
     'JWT_AUDIENCE': 'AUDIENCE', # in your test may be http://localhost:3000
     'JWT_ISSUER': 'YOUR_ISSUER', # see below for more info
