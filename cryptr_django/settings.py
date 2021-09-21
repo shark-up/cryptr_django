b687918bdd688b0c4cdc627426ae9162a549e490
Thibaud RENAUX <thibaud.renaux@gmail.com>
2021-09-21 17:29:52 +0200

    gitguardian

--- cryptr_django/settings.py
+++ cryptr_django/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')m$^+xra99wh%v^6+z$=fykr40kqor9#o%c@#v7-1q=2g&iwkr'
+SECRET_KEY = 'DJANGO_SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
